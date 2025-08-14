65b8622574c90047a48013e1b0403060f0db8e00
Ben Manning <bmanning@me.com>
2025-08-14 12:18:58 -0400

    complete read index functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
-// src/App.jsx
+import { useState, useEffect } from 'react';
+import * as petServices from './services/petService.js';
+
+import PetList from './components/PetList/PetList.jsx';
+
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [pets, setPets] = useState([]);
+
+  useEffect(() => {
+    const fetchPets = async () => {
+      try {
+        const fetchedPets = await petServices.index();
+
+        if (fetchedPets.err) {
+          throw new Error(fetchedPets.err);
+        }
+
+        setPets(fetchedPets);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    fetchPets();
+  }, []);
+
+
+  return (
+    <>
+      <PetList pets={pets} />
+    </>
+  )
 };
 
 export default App;
